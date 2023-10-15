bf8222fb23b5613480ed165afbe8fd59c985cd8c
GitItBert <77079400+GitItBert@users.noreply.github.com>
2023-10-15 00:37:26 -0700

    Reorganized directories

---
+++ client1/client.py
@@ -0,0 +1,43 @@
+import socket
+import threading
+import os
+
+# Client configuration
+HOST = 'localhost'
+PORT = 12345
+
+def send_message(client_socket):
+    while True:
+        message = input()
+        if message.lower() == "exit":
+            break
+
+        if os.path.isfile(message):
+            # Send a file
+            client_socket.send(f'FILE:{message}'.encode())
+            with open(message, 'rb') as file:
+                file_data = file.read(1024)
+                while file_data:
+                    client_socket.send(file_data)
+                    file_data = file.read(1024)
+        else:
+            # Send a message
+            client_socket.send(message.encode())
+
+# Create a socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+# Start a thread to send messages
+send_thread = threading.Thread(target=send_message, args=(client_socket,))
+send_thread.start()
+
+# Receive and display messages from the server
+while True:
+    data = client_socket.recv(1024)
+    if not data:
+        break
+
+    print(data.decode())
+
+client_socket.close()
